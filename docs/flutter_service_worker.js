9b817412af771fdbaaa153a2f2b6bb1774ab89ff
Raghava Reddy S <105329592+unknownuser44106@users.noreply.github.com>
2025-07-04 10:36:39 +0530

    Add web build to docs for GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -80,7 +80,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1815af0fc3c01a3c7ab167a6073d2b14
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "301121c9cf25071f57ca20543b4ee15f",
+"flutter_bootstrap.js": "c7e982861cf8fcc04fff1bf7af846065",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
