8fccbb7df13815575a3347bb4a35db98d1986f90
Raghava Reddy S <105329592+unknownuser44106@users.noreply.github.com>
2025-07-04 17:06:07 +0530

    Update for GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "299734091"
+    serviceWorkerVersion: "2329684198"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -80,7 +80,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1815af0fc3c01a3c7ab167a6073d2b14
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "09d18dbfdac5e8a626b89c5512ea332f",
+"flutter_bootstrap.js": "04f96690be6cc4bafcfa7e5ddb43dca7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
